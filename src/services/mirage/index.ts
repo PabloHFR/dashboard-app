3c21803476cf94e9bbc6aa69ae8917095f744e86
Pablo H. F. Roscoche <pablo.dkz2@gmail.com>
2023-03-14 11:53:51 -0300

    Added create user feature

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,10 @@
-import { createServer, Factory, Model, Response } from "miragejs";
+import {
+  createServer,
+  Factory,
+  Model,
+  Response,
+  RestSerializer,
+} from "miragejs";
 
 declare module "miragejs/server" {
   interface Server {
@@ -14,8 +20,12 @@ type User = {
 
 export function makeServer() {
   const server = createServer({
+    serializers: {
+      application: RestSerializer,
+    },
+
     models: {
-      user: Model.extend<Partial<User>>({}),
+      user: Model,
     },
 
     factories: {
@@ -26,14 +36,14 @@ export function makeServer() {
         email(i) {
           return `user${i + 1}@gmail.com`;
         },
-        createdAt() {
-          return `2023-02-12T00:00:00.000Z`;
+        createdAt(i) {
+          return `2023-02-1${i}T00:00:00.000Z`;
         },
       }),
     },
 
     seeds(server) {
-      server.createList("user", 200);
+      server.createList("user", 3);
     },
 
     routes() {
@@ -65,7 +75,16 @@ export function makeServer() {
         }
       );
       this.get("/users/:id");
-      this.post("/users");
+      this.post("/users", (schema, request) => {
+        let attrs = JSON.parse(request.requestBody);
+
+        return schema.create("user", {
+          createdAt: attrs.user.created_at,
+          email: attrs.user.email,
+          name: attrs.user.name,
+          id: attrs.id,
+        });
+      });
 
       this.namespace = "";
       this.passthrough();
